ac349bde97cae48f5b60d2d5c74194c092895e11
karlicoss <devnull@localhost>
2013-05-11 18:54:51 +0400

    markdown

--- init.el
+++ init.el
@@ -91,4 +91,8 @@
 ; Markdown
 ; ========
 (require 'markdown-mode)
+(autoload 'markdown-mode "markdown-mode" "Major mode for editing Markdown files" t)
 (setq markdown-enable-math t)
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
